821e8f01d1f4cdecdee96a25676d8d371d0035a5
Dan Novac <danynovac@gmail.com>
2020-11-05 22:30:22 +0200

    Small Refactoring

--- src/com/dnovac/Main.java
+++ src/com/dnovac/Main.java
@@ -1,29 +1,112 @@
 package com.dnovac;
 
+import com.dnovac.search.BinarySearch;
+import com.dnovac.search.QuickSort;
+import com.dnovac.structure.MyLinkedList;
 import com.dnovac.tree.BinaryTreeImpl;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.random;
 
 
 public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("######## begin ############");
+    int[] numbers = {1, 20, 22, 102, 101, 1221, 13321, 13331, 0, 11};
+
+    checkPalindromes(numbers);
     playLinkedListImplementation();
-    System.out.println("######### end #############");
 
     BinaryTreeImpl.Node root = new BinaryTreeImpl.Node(6);
-    BinaryTreeImpl binaryTree = new BinaryTreeImpl(root);
-    binaryTree.add(4);
-    binaryTree.add(8);
-    binaryTree.add(3);
-    binaryTree.add(9);
-    binaryTree.add(5);
-    binaryTree.add(7);
+    BinaryTreeImpl binaryTree = buildBinaryTree(root);
 
-    //traversions
+    traverseBinaryTreeInAllTheOrders(root);
+
+    // sum of tree
+    System.out.println(String.format("========= The sum of tree elements is : %s ================", BinaryTreeImpl.sumOfTreeNodes(root)));
+
+    //anagram
+    boolean anagram = anagram("mary", "army");
+    System.out.println(String.format("Is anagram? [%s]", anagram));
+
+    //binarysearch
+    System.out.println("========== Binary Search List ===========");
+    int[] array = IntStream.range(5, 100).toArray();
+    BinarySearch.binarySearch(array, 0, array.length - 1, 66);
+
+    //BFS
+    System.out.println("========= BFS =========");
+    binaryTree.breadthFirstSearch(5);
+
+    //DFS
+    System.out.println("========= DFS =========");
+    binaryTree.depthFirstSearch(7);
+
+    //Lowest common ancestor
+    System.out.println("========= Least Common Ancestor =========");
+    binaryTree.lowestCommonAncestor(root, 9, 4);
+
+    reverseLinkedList();
+
+    //Quicksort
+    int[] randomArray = IntStream.range((int) random(), 100).toArray();
+    List<Integer> randomList = quickSortArray(randomArray);
+
+    int maximumNumber = randomList.stream().mapToInt(value -> value).max().orElseThrow(NoSuchElementException::new);
+    int maximumIndex = randomList.indexOf(maximumNumber);
+    System.out.println(String.format("MAX is %s at index %s", maximumNumber, maximumIndex));
+
+    //reverse tree
+    binaryTree.reverseTreeNodes(root);
+    System.out.println("========= Reversed tree inOrder ===========");
+    BinaryTreeImpl.inOrder(root);
+
+    System.out.println(String.format("\nMAX height of tree is: %s", binaryTree.maxHeight(root)));
+    System.out.println(String.format("\n Height of tree for input data is: %s", binaryTree.heightOfNode(root, 9, 1)));
+
+    System.out.println("===== PRINT NODES AT K DISTANCE FROM ROOT =====");
+    BinaryTreeImpl.printKDistant(root, 2);
+  }
+
+  private static List<Integer> quickSortArray(int[] randomArray) {
+
+    List<Integer> randomList = Arrays.stream(randomArray).boxed().collect(Collectors.toList());
+    Collections.shuffle(randomList);
+    Integer[] toBeSortedInteger = randomList.toArray(new Integer[randomList.size()]);
+    int[] toBeSorted = new int[randomList.size()];
+
+    for (int i = 0; i < toBeSorted.length; i++) {
+      toBeSorted[i] = toBeSortedInteger[i];
+    }
+    System.out.println("Array to be quicksorted is: " + Arrays.toString(toBeSorted));
+
+    QuickSort quickSort = new QuickSort();
+    quickSort.sort(toBeSorted);
+    System.out.println("Sorted arrays is: " + Arrays.toString(toBeSorted));
+    return randomList;
+  }
+
+  private static void reverseLinkedList() {
+
+    LinkedList<Integer> linkedList = new LinkedList<>(Arrays.asList(1, 5, 6, 3, 4, 2, 9));
+    Collections.reverse(linkedList);
+    System.out.println("Reversed Linked list: ");
+    linkedList.forEach(s -> System.out.print(s + ", "));
+    linkedList.sort(Comparator.naturalOrder());
+    linkedList.forEach(s -> System.out.println(s + ", "));
+  }
+
+  private static void traverseBinaryTreeInAllTheOrders(BinaryTreeImpl.Node root) {
+
     System.out.println("=========Left-Node-Right========");
     BinaryTreeImpl.inOrder(root);
 
@@ -32,24 +115,37 @@ public class Main {
 
     System.out.println("\n=========Left-Right-Node========");
     BinaryTreeImpl.postOrder(root);
+  }
 
-    // sum of tree
-    int sumOfTreeNodes = BinaryTreeImpl.sumOfTreeNodes(root);
-    System.out.println("========= The sum of tree elements is : " + sumOfTreeNodes + " ================");
+  private static BinaryTreeImpl buildBinaryTree(BinaryTreeImpl.Node root) {
 
-    //anagram
-    System.out.println("is anagram? " + anagram("mary", "army"));
+    BinaryTreeImpl binaryTree = new BinaryTreeImpl(root);
+    binaryTree.add(4);
+    binaryTree.add(8);
+    binaryTree.add(3);
+    binaryTree.add(9);
+    binaryTree.add(5);
+    binaryTree.add(7);
+    return binaryTree;
+  }
 
+  private static void checkPalindromes(int[] numbers) {
+
+    for (int number : numbers) {
+      System.out.println(String.format("Is %s palindrome ? %s", number, isPalindrome(number)));
+    }
   }
 
   private static void playLinkedListImplementation() {
 
+    System.out.println("######## Linked List ############");
     MyLinkedList myLinkedList = new MyLinkedList();
-    myLinkedList = MyLinkedList.addToTail(myLinkedList, 10);
-    myLinkedList = MyLinkedList.addToTail(myLinkedList, 20);
-    myLinkedList = MyLinkedList.addToTail(myLinkedList, 15);
-    myLinkedList = MyLinkedList.addToTail(myLinkedList, 18);
+    MyLinkedList.addToTail(myLinkedList, 10);
+    MyLinkedList.addToTail(myLinkedList, 20);
+    MyLinkedList.addToTail(myLinkedList, 15);
+    MyLinkedList.addToTail(myLinkedList, 18);
     MyLinkedList.printList(myLinkedList);
+    System.out.println("######### end LinkedList #############");
   }
 
   private static boolean anagram(String s1, String s2) {
@@ -66,8 +162,26 @@ public class Main {
     System.out.println(s1Characters);
     System.out.println(s2Characters);
     return s1Characters.equals(s2Characters);
+  }
 
+  private static boolean isPalindrome(int number) {
 
+    StringBuilder stringBuilder = new StringBuilder(number);
+    if (stringBuilder.length() > 0) {
+      int reverse = Integer.parseInt(stringBuilder.reverse().toString());
+      return reverse == number;
+    }
+    return false;
+  }
+
+  private static int reverse(int number) {
+
+    int reverse = 0;
+    while (number != 0) {
+      reverse = reverse * 10 + number % 10;
+      number = number / 10;
+    }
+    return reverse;
   }
 
 }
---
+++ src/com/dnovac/search/BinarySearch.java
@@ -0,0 +1,76 @@
+package com.dnovac.search;
+
+/**
+ * @author Dan Novac on 31/10/2020
+ * @project algorithms-playground
+ */
+public class BinarySearch {
+
+  /**
+   * =========== BINARY SEARCH ==============
+   * <p>
+   * In the case of recursive binary search implementation,
+   * we calculate the middle position by taking the start and end position and
+   * check if the target element is equal to the middle element or not.
+   * <p>
+   * If the target, the number of the element you are searching in an array is equal then our search is complete,
+   * but if the target is greater than middle we look on the second half of array and if the target is less than middle element
+   * then we look into the first half of array.
+   * <p>
+   * This is possible because in the case of binary search the array is always sorted if it's not,
+   * you must sort the array before conducting a binary search.
+   * <p>
+   * So, in each iteration, the value of start and end position changes like at first,
+   * start=0 and end=length-1
+   * but then depending upon the value of target element we move the pointer to the first or second half of array.
+   * <p>
+   * This gives you the base case I mean,.
+   * since the array is getting smaller and smaller in every iteration at one point it will limit to just one element
+   * and after that end will be less than the start.
+   * <p>
+   * At this point, you can stop the binary search because now you cannot divide the array further,
+   * which means the element doesn't exist in the array. Our solution returns -1 at this point in time.
+   * <p>
+   * Read more: https://www.java67.com/2016/10/binary-search-using-recursion-in-java.html#ixzz6cSUskG75
+   */
+
+  public static int binarySearch(int[] array, int start, int end, int target) {
+
+    if (start > end) {
+      return -1;
+    }
+
+    int middle = (start + end) / 2;
+
+    if (array[middle] == target) {
+      System.out.println("Target found at index: " + middle);
+      return middle;
+    } else if (target > array[middle]) {
+      //it's in the right side
+      return binarySearch(array, middle + 1, array.length, target);
+    } else {
+      //left side
+      return binarySearch(array, start, middle - 1, target);
+    }
+  }
+
+  public static int binarySearchWithoutRecursion(int[] array, int target) {
+
+    int start = 0;
+    int end = array.length - 1;
+
+    while (start <= end) {
+      int middle = (start + end) / 2;
+      if (target == array[middle]) {
+        System.out.println("Value found on index: " + middle);
+        return middle;
+      } else if (target < array[middle]) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+    return -1;
+  }
+
+}
